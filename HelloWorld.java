9464860e73e8dd7ac8c401e8e771760eb16d1357
Caroline Eliasson <eliasson.caroline@gmail.com>
2020-12-26 12:32:14 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 /*My first Java program: The traditional "Hello World".*/
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.print("Hello World!");
+    public static void main(String[] args) { /*This is where the program starts */
+        System.out.print("Hello World!");  /*Program "Hello World" is printed/written on user screen. */
     }
 }
